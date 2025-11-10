f78d50e4a33f5db9adaf34f8f981bc6a0f6fadab
taujago <taujago@gmail.com>
2025-11-10 18:24:12 +0800

    fix: test date and time

--- dashboard-operator/routes/web.php
+++ dashboard-operator/routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PertandinganController;
 
 Route::get('/', function () {
-    return dd(date('Y-m-d h:i:s'));
+    return dd(date('Y-m-d H:i:s'));
 });
 
 Route::get('/dashboard', function () {
